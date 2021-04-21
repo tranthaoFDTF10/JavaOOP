0ae1f8e009c1597a2f2419931b132222aacff48b
Tran Thao <79261080+tranthaoFDTF10@users.noreply.github.com>
2021-04-21 21:00:48 +0700

    Update SDDiemMau.java

--- JavaOOP_buoi4/src/javaoop_buoi4/SDDiemMau.java
+++ JavaOOP_buoi4/src/javaoop_buoi4/SDDiemMau.java
@@ -9,6 +9,7 @@ package javaoop_buoi4;
  *
  * @author Administrator
  */
+import java.util.Scanner;
 public class SDDiemMau {
     public static void main(String[] args){
         DiemMau A = new DiemMau(5, 10, "Trang");
@@ -25,6 +26,29 @@ public class SDDiemMau {
         
         System.out.println("Khoang cach diem B den truc hoanh la: "+B.khoangcach());
         
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so luong diem: ");
+        int n = sc.nextInt();
+        Diem ds[] = new Diem[n]; // tao ra mang co n tham chieu toi lop Diem
+        for(int i=0;i<n;i++){
+            System.out.println("1. Nhap diem.");
+            System.out.println("2. Nhap diem mau.");
+            System.out.print("=> Ban muon nhap diem nao? Hay nhap so tuong ung: ");
+            int c = sc.nextInt();
+            System.out.println("Nhap diem thu "+(i+1)+": ");
+            if(c==1){
+                ds[i] = new Diem();
+                ds[i].nhapDiem();
+            }
+            if(c==2){
+                ds[i] = new DiemMau();
+                ds[i].nhapDiem();
+            }
+            
+        }
+        for(int i=0;i<n;i++){
+            System.out.println("Thong tin diem thu "+(i+1)+": "+ds[i].toString());
+        }
         
     }
 }
